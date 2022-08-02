2cf3f78fbf2cabdd5d56b712fb5a66d420110659
zjoney <zdjoey@126.com>
2022-08-02 13:44:34 +0800

    💥 feat(): 请求或者储存方法

--- js/background.js
+++ js/background.js
@@ -65,3 +65,96 @@ chrome.contextMenus.onClicked.addListener((clickData) => {
     }
   }
 })
+
+/**
+ * 获取本地存储
+ */
+ const getUserList = () => {
+  return new Promise((resolve, reject) => {
+    chrome.storage.sync.get('userlist', (arg) => {
+      resolve(arg.hasOwnProperty('userlist') ? arg.userlist : [])
+    })
+  })
+}
+
+/**
+ * 添加 本地存储
+ */
+const setStorage = (userListItem, tabId) => {
+  return new Promise((resolve, reject) => {
+    getUserList().then((userlist) => {
+      let flag = true
+      const { user_id } = userListItem
+      // 判断是否存在相同的用户，有则更新
+      for (let i = 0; i < userlist.length; i++) {
+        if (userlist[i].user_id === user_id) {
+          userlist[i] = userListItem
+          flag = false
+        }
+      }
+
+      // 没有则push
+      if (flag) userlist.push(userListItem)
+
+      // 进行存储
+      chrome.storage.sync.set({ userlist: userlist })
+
+
+      // 判断入口，是 手动录入 还是 右键添加
+      if (tabId) {
+        sendDataPopup(tabId)
+      } else {
+        sendData()
+      }
+      resolve()
+    })
+  })
+}
+
+/**
+ * 进行请求
+ * @param {*} tabId 页面id
+ */
+const requestUserInfo = (tabId = 0) => {
+  const reqUrl = `https://api.juejin.cn/user_api/v1/user/get?aid=${reqQuery.aid}&uuid=${reqQuery.uuid}&user_id=${reqQuery.user_id}&not_self=${reqQuery.not_self}&need_badge=${reqQuery.need_badge}`
+
+  return new Promise((resolve, reject) => {
+    fetch(reqUrl)
+      .then((response) => response.text())
+      .then((text) => {
+        const resObj = JSON.parse(text)
+        const { user_name, avatar_large, user_id } = resObj.data
+        const userListItem = {
+          user_name,
+          avatar_large,
+          user_id
+        }
+        console.log(userListItem, '@@')
+        setStorage(userListItem, tabId)
+          .then(() => {
+            resolve()
+          })
+          .catch((error) => {
+            reject(2)
+          })
+      })
+      .catch((error) => {
+        console.log(error)
+        reject(1)
+      })
+  })
+}
+
+
+/**
+ * 取消 特别关注
+ */
+ const cnacelFollowUserInfo = (user_id) => {
+  getUserList().then((userlist) => {
+    const newUserList = userlist.filter((item) => {
+      return item.user_id !== user_id
+    })
+    chrome.storage.sync.set({ userlist: newUserList })
+    sendData()
+  })
+}
